6a1c134a164897a5e791c2e05ec1329ec0e3d707
JGabriel963 <joaogabriel9633@gmail.com>
2022-12-17 15:34:03 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 49.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição Simples
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
