e6784957c014de6118da3ad3c3bca54d2df262c1
JGabriel963 <joaogabriel9633@gmail.com>
2022-12-30 00:02:00 -0300

    updatelamp

--- projeto-lampada/js/script.js
+++ projeto-lampada/js/script.js
@@ -1,28 +1,60 @@
-const ligar = document.querySelector(".on");
-const desligar = document.querySelector(".off")
+const ligarDesligar = document.querySelector(".on");
 const lampada = document.querySelector(".img")
+
+function isLampBroken () {
+    return lampada.src.indexOf('quebrada') > -1
+}
 
 
 function lampBrok() {
     lampada.src = "img/quebrada.jpg"
+
+    setTimeout(function() {
+        alert("Clique duas vezes no botão ou atualize a página para concertar a lâmpada.")
+    }, 250)
 }
 
+
 function lampOn() {
-    if (! lampBrok) {
+    if (!isLampBroken()) {
         lampada.src = "img/ligada.jpg"
     }
 }
 
 function lampOff() {
-    lampada.src = "img/desligada.jpg"
+    if (!isLampBroken()) {
+        lampada.src = "img/desligada.jpg"
+    } 
+}
+    
+
+function lampBroken() {
+    lampada.src = "img/quebrada.jpg"
 }
 
+function lampOnOff() {
+    if(ligarDesligar.textContent == 'Ligar') {
+        lampOn();
+        ligarDesligar.textContent = 'Desligar'
+        ligarDesligar.classList.toggle("off")
+    } else {
+        lampOff();
+        ligarDesligar.textContent = 'Ligar'
+        ligarDesligar.classList.toggle("off")
+    }
+}
 
+function Concert() {
+    if(isLampBroken) {
+        lampada.src = "img/desligada.jpg"
+    }
+}
 
-
-
-ligar.addEventListener("click", lampOn)
-desligar.addEventListener("click", lampOff)
+ligarDesligar.addEventListener("click", lampOnOff)
+ligarDesligar.addEventListener("dblclick", Concert)
 lampada.addEventListener("dblclick", lampBrok)
+lampada.addEventListener("mouseenter", lampOn)
+lampada.addEventListener("mouseleave", lampOff)
+lampada.addEventListener("dblclick", lampBroken)
 
 
